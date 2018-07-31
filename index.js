0e3043be1d263266165142e52ef38896d14b928f
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-31 11:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const app = "I don't do much."
 
-var dodger = document.getElementById('dodger');
 
-
-
-document.addEventListener('keydown', function(key) {
-  if (key===39){
-    var rightNumbers = dodger.style.right.replace('px', '');
-    var right = parseInt(rightNumbers, 10);
-    dodger.style.right = `${right - 1}px`
-  }
-});
 
 
 
